117038c19990b303a9326bcddee432dad444d798
jss <jss9960@hotmail.com>
2020-01-14 20:44:43 -0800

    added php file

--- js/main.js
+++ js/main.js
@@ -2,6 +2,22 @@ var app = new Vue({
     el: '#app',
     data: {
       message: 'que onda soy el nuevo mensaje',
-      portfolioElements: 
+      portfolioElements: []
+    },
+    methods:{
+      callPortfolio: function(){
+        let self = this;
+        $.ajax({
+          url: "https://curada.000webhostapp.com/js/json/portfolio.json", 
+          crossDomain : true,
+          success: function(result){
+            self.portfolioElements = result;
+            console.log(result);
+        }});
+      }
+    },
+    created: function(){
+      let self = this;
+      self.callPortfolio();
     }
   })
