18d50430d8c9766fc3477438274ae53f42fdd66f
jss <jss9960@hotmail.com>
2020-01-14 21:31:17 -0800

    fixed img points

--- hello.php
+++ hello.php
@@ -2,27 +2,27 @@
 header('Access-Control-Allow-Origin: *');
 echo "[
     {
-    \"img:\": \"img/portfolio/elemento1.jpg\",
+    \"img\": \"img/portfolio/elemento1.jpg\",
     \"text\": \"elemento1\",
     \"isActive\": true
     }, {
-    \"img:\": \"img/portfolio/elemento2.jpg\",
+    \"img\": \"img/portfolio/elemento2.jpg\",
     \"text\": \"elemento1\",
     \"isActive\": true
     }, {
-    \"img:\": \"img/portfolio/elemento3.jpg\",
+    \"img\": \"img/portfolio/elemento3.jpg\",
     \"text\": \"elemento1\",
     \"isActive\": true
     }, {
-    \"img:\": \"img/portfolio/elemento3.jpg\",
+    \"img\": \"img/portfolio/elemento3.jpg\",
     \"text\": \"elemento1\",
     \"isActive\": true
     }, {
-    \"img:\": \"img/portfolio/elemento3.jpg\",
+    \"img\": \"img/portfolio/elemento3.jpg\",
     \"text\": \"elemento1\",
     \"isActive\": true
     }, {
-    \"img:\": \"img/portfolio/elemento3.jpg\",
+    \"img\": \"img/portfolio/elemento3.jpg\",
     \"text\": \"elemento1\",
     \"isActive\": true
     }
--- js/main.js
+++ js/main.js
@@ -1,22 +1,32 @@
+const BASEURL = "https://curada.000webhostapp.com/";
+
 var app = new Vue({
     el: '#app',
     data: {
       message: 'que onda soy el nuevo mensaje',
-      portfolioElements: []
+      portfolioElements: [],
+      baseUrl: BASEURL
     },
     methods:{
       callPortfolio: function(){
         let self = this;
         $.ajax({
-          url: "https://curada.000webhostapp.com/hello.php", 
+          url: self.baseUrl + "/hello.php", 
           crossDomain : true,
           datatype: 'json',
           success: function(result){
-            console.log(result)
             self.portfolioElements = JSON.parse(result)
             
             console.log(self.portfolioElements);
         }});
+      },
+      GetImageURL: function(url){
+        let self = this;
+        if(url.img != undefined){
+          return self.baseUrl + url.img;
+        }else{
+          return "http://qnimate.com/wp-content/uploads/2014/03/images2.jpg"
+        }
       }
     },
     created: function(){
