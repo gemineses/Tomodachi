6c3ce19735e6540421e47be278bd58175fab755e
jss <jss9960@hotmail.com>
2020-01-14 20:25:42 -0800

    added json

--- js/main.js
+++ js/main.js
@@ -2,32 +2,6 @@ var app = new Vue({
     el: '#app',
     data: {
       message: 'que onda soy el nuevo mensaje',
-      portfolioElements: [
-          {
-            img: 'img/portfolio/elemento1.jpg',
-            text: 'elemento1',
-            isActive: true
-          }, {
-            img: 'img/portfolio/elemento2.jpg',
-            text: 'elemento2',
-            isActive: true
-          }, {
-            img: 'img/portfolio/elemento3.jpg',
-            text: 'elemento2',
-            isActive: true
-          }, {
-            img: 'img/portfolio/elemento3.jpg',
-            text: 'elemento2',
-            isActive: true
-          }, {
-            img: 'img/portfolio/elemento3.jpg',
-            text: 'elemento2',
-            isActive: true
-          }, {
-            img: 'img/portfolio/elemento3.jpg',
-            text: 'elemento2',
-            isActive: true
-          }
-        ]
+      portfolioElements: 
     }
   })
